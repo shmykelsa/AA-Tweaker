17d08ecb9ed3df80d0972b9ef82da50823fd9074
Gabriele Rizzo <gabriele.rizzo@outlook.com>
2021-01-20 08:28:06 +0100

    RevertDialog: refactor for translations

--- app/src/main/java/sksa/aa/tweaker/RevertDialog.java
+++ app/src/main/java/sksa/aa/tweaker/RevertDialog.java
@@ -6,20 +6,18 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 
-import static android.system.Os.kill;
-
 
 public class RevertDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("Do you want to revert everything? Please manually restart the app after doing so")
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        builder.setMessage(getString(R.string.revert_everything_dialog))
+                .setPositiveButton(getString(R.string.positive_button), new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         MainActivity.getAndRemoveOptionsSelected();
                     }
                 })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                .setNegativeButton(getString(R.string.negative_button), new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         dialog.cancel();
                     }
